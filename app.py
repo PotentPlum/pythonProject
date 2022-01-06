0cad8abad00a6d110ee258fe9303d0de274f79a4
Ernst Honkoop <ernsthonkoop93@hotmail.com>
2022-01-06 18:11:33 +0100

    Initial commit

---
+++ app.py
@@ -0,0 +1,9 @@
+import urllib
+import webbrowser
+import time
+
+webbrowser.open('https://www.google.nl')
+time.sleep(1)
+webbrowser.open('https://www.youtube.com')
+time.sleep(1)
+webbrowser.open('https://www.reddit.com')
